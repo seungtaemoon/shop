d63abea93702c45535e406e34ebc1fb7d6998686
seungtaemoon <Robbie@email.com>
2023-09-04 11:34:57 +0900

    회원가입 기능 구현

---
+++ src/main/java/com/sparta/myselectshop/dto/SignupRequestDto.java
@@ -0,0 +1,21 @@
+package com.sparta.myselectshop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+    @Email
+    @NotBlank
+    private String email;
+    private boolean admin = false;
+    private String adminToken = "";
+}
