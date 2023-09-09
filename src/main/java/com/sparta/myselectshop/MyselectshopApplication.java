8315a5fbc56f7d74503371547a0228c6b20b6d77
seungtaemoon <Robbie@email.com>
2023-09-09 15:25:38 +0900

    Controller 테스트

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
 public class MyselectshopApplication {
 
     public static void main(String[] args) {
